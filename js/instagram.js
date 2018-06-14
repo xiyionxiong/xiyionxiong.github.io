16413e641857819af646a0acd8c6095d3ed3855c
熊凯 <xiongkai@wiseasy.com>
2018-06-14 22:09:56 +0800

    Site updated: 2018-06-14 22:09:55

--- js/instagram.js
+++ js/instagram.js
@@ -1,10 +1,10 @@
-var Instagram = (function(){
+var Instagram = (function () {
 
 	var _collection = [];
 
-	var preLoad = function(data){
-		for(var em in data){
-			for(var i=0,len=data[em].srclist.length;i<len;i++){
+	var preLoad = function (data) {
+		for (var em in data) {
+			for (var i = 0, len = data[em].srclist.length; i < len; i++) {
 				var src = data[em].bigSrclist[i];
 				var img = new Image();
 				img.src = src;
@@ -12,141 +12,152 @@ var Instagram = (function(){
 		}
 	}
 
-	var render = function(data){
-		for(var em in data){
+	// data[em].bigSrclist[i]
+	var render = function (data) {
+		for (var em in data) {
 			var liTmpl = "";
-			for(var i=0,len=data[em].srclist.length;i<len;i++){
+			for (var i = 0, len = data[em].srclist.length; i < len; i++) {
 				liTmpl += '<li>\
 								<div class="img-box">\
-									<a class="img-bg" target="_blank" rel="example_group" href="'+data[em].bigSrclist[i]+'" title="'+data[em].text[i]+'"></a>\
-									<img src="'+data[em].srclist[i]+'" alt="">\
+									<a class="img-bg" target="_blank" rel="example_group" href="' + data[em].linkList[i] + '" title="' + data[em].text[i] + '"></a>\
+									<img src="' + data[em].srclist[i] + '" alt="">\
 								</div>\
 							</li>';
 			}
-			$('<section class="archives album"><h1 class="year">'+data[em].year+'年<em>'+data[em].month+'月</em></h1>\
-				<ul class="img-box-ul">'+liTmpl+'</ul>\
+			$('<section class="archives album"><h1 class="year">' + data[em].year + '年<em>' + data[em].month + '月</em></h1>\
+				<ul class="img-box-ul">' + liTmpl + '</ul>\
 				</section>').appendTo($(".instagram"));
 		}
 
-		$(".instagram").lazyload();
+		// $(".instagram").lazyload();
 		changeSize();
 
-		setTimeout(function(){
+		setTimeout(function () {
 			preLoad(data);
-		},3000);
-		
-		$("a[rel=example_group]").fancybox();
+		}, 3000);
+
+		// $("a[rel=example_group]").fancybox();
 	}
 
-    //这是一个 将图片转到墙内的方法
-	var replacer = function(str){
-		if(str.indexOf("outbound-distilleryimage") >= 0 ){
+	//这是一个 将图片转到墙内的方法
+	var replacer = function (str) {
+		if (str.indexOf("outbound-distilleryimage") >= 0) {
 			var cdnNum = str.match(/outbound-distilleryimage([\s\S]*?)\//)[1];
 			var arr = str.split("/");
-			return "http://distilleryimage"+cdnNum+".ak.instagram.com/"+arr[arr.length-1];
-		}else{
+			return "http://distilleryimage" + cdnNum + ".ak.instagram.com/" + arr[arr.length - 1];
+		} else {
 			var url = "http://photos-g.ak.instagram.com/hphotos-ak-xpf1/";
 			var arr = str.split("/");
-			return url+arr[arr.length-1];
+			return url + arr[arr.length - 1];
 		}
 	}
 
-	var ctrler = function(data){
+	var ctrler = function (data) {
 		var imgObj = {};
-		for(var i=0,len=data.length;i<len;i++){
-			var d = new Date(data[i].created_time*1000);
+		for (var i = 0, len = data.length; i < len; i++) {
+			var d = new Date(data[i].created_time * 1000);
 			var y = d.getFullYear();
-			var m = d.getMonth()+1;
+			var m = d.getMonth() + 1;
 			// var src = replacer(data[i].images.low_resolution.url);
-            	var src = data[i].images.low_resolution.url;
+			var src = data[i].images.low_resolution.url;
+			let link = data[i].link;
 			// var bigSrc = replacer(data[i].images.standard_resolution.url);
-            	var bigSrc = data[i].images.standard_resolution.url;
+			var bigSrc = data[i].images.standard_resolution.url;
 			var text = data[i].caption ? data[i].caption.text : '';
-			var key = y+"-"+m;
-			if(imgObj[key]){
+			var key = y + "-" + m;
+			if (imgObj[key]) {
 				imgObj[key].srclist.push(src);
 				imgObj[key].bigSrclist.push(bigSrc);
 				imgObj[key].text.push(text);
-			}else{
+				imgObj[key].linkList.push(link);
+			} else {
 				imgObj[key] = {
-					year:y,
-					month:m,
-					srclist:[bigSrc],
-					bigSrclist:[bigSrc],
-					text:[text]
+					year: y,
+					month: m,
+					srclist: [bigSrc],
+					bigSrclist: [bigSrc],
+					linkList: [link],
+					text: [text]
+					// link: link
 				}
 			}
+			// console.info("link:====>", link);
 		}
+
+
 		render(imgObj);
+
 	}
 
-	var getList = function(url){
+	var getList = function (url) {
 		$(".open-ins").html("图片来自instagram，正在加载中…");
 		$.ajax({
 			url: url,
-			type:"GET",
-			dataType:"jsonp",
-			success:function(re){
-				if(re.meta.code == 200){
+			type: "GET",
+			dataType: "jsonp",
+			success: function (re) {
+				if (re.meta.code == 200) {
 					_collection = _collection.concat(re.data);
 					var next = re.pagination.next_url;
-					if(next){
+					if (next) {
 						getList(next);
-					}else{
+					} else {
 						$(".open-ins").html("图片来自instagram，点此访问");
 						ctrler(_collection);
 					}
-				}else{
+				} else {
 					alert("access_token timeout!");
 				}
 			}
 		});
 	}
-	
 
-    //不需要改变大小
-	var changeSize = function(){	
-		if($(document).width() <= 600){
-			$(".img-box").css({"width":"auto", "height":"auto"});
-		}else{
+
+	//不需要改变大小
+	var changeSize = function () {
+		if ($(document).width() <= 600) {
+			$(".img-box").css({
+				"width": "auto",
+				"height": "auto"
+			});
+		} else {
 			var width = $(".img-box-ul").width();
-			var size = Math.max(width*0.26, 157);
+			var size = Math.max(width * 0.26, 157);
 			$(".img-box").width(size).height(size);
 		}
 	}
 
-	var bind = function(){
-		$(window).resize(function(){
+	var bind = function () {
+		$(window).resize(function () {
 			changeSize();
 		});
 	}
 
 	return {
-		init:function(){
+		init: function () {
 			var insid = $(".instagram").attr("data-client-id");
-            var userId = $(".instagram").attr("data-user-id");
-			if(!insid){
+			var userId = $(".instagram").attr("data-user-id");
+			if (!insid) {
 				alert("Didn't set your instagram client_id.\nPlease see the info on the console of your brower.");
 				console.log("Please open 'http://instagram.com/developer/clients/manage/' to get your client-id.");
 				return;
 			}
-            	getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f686a42.cd584bf27854444db0c658e4dcfc7375&count=100");
-            	// getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f686a42.cd584bf27854444db0c658e4dcfc7375&count=100");
+			getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f686a42.cd584bf27854444db0c658e4dcfc7375&count=100");
+			// getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f686a42.cd584bf27854444db0c658e4dcfc7375&count=100");
 			// getList("https://api.instagram.com/v1/users/" + userId + "/media/recent/?client_id="+insid+"&count=100");
 			bind();
 		}
 	}
 })();
-$(function(){
+$(function () {
 	Instagram.init();
 })
 
 
-		// getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f25912c.e5787853ef834a5c924aca0d6658e45d&count=100");
-            
+// getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f25912c.e5787853ef834a5c924aca0d6658e45d&count=100");
 
-     //  	 getList("https://api.instagram.com/v1/users/2723120904/media/recent/?client_id=f686a421cbfb41bcb96aa2507e09671d&count=100");
-		
-       // access_token=2723120904.f686a42.cd584bf27854444db0c658e4dcfc7375
-        //	getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f25912c.e5787853ef834a5c924aca0d6658e45d&count=100");
-        
+
+//  	 getList("https://api.instagram.com/v1/users/2723120904/media/recent/?client_id=f686a421cbfb41bcb96aa2507e09671d&count=100");
+
+// access_token=2723120904.f686a42.cd584bf27854444db0c658e4dcfc7375
+//	getList("https://api.instagram.com/v1/users/2723120904/media/recent/?access_token=2723120904.f25912c.e5787853ef834a5c924aca0d6658e45d&count=100");
